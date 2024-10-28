4bf1eef19fc3a4166ad61774769e468ae4a3bc9e
Fernando-Jose-Claros-Orellana <ferinocente357@gmail.com>
2024-10-28 08:14:47 -0600

    Se agrega clase para la prueba

---
+++ EISGFJCO20241028Test/EISGFJCO20241028PruebaUI.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EISGFJCO20241028Test
+{
+	[TestClass]
+	public class EISGFJCO20241028PruebaUI : IDisposable
+	{
+		private IWebDriver driver;
+
+		public EISGFJCO20241028PruebaUI()
+		{
+			// Inicializa el controlador de Chrome
+			driver = new ChromeDriver();
+		}
+
+		[TestMethod]
+		public void Blazor()
+		{
+			// Navega a Google
+			driver.Navigate().GoToUrl("https://esfebodega.netlify.app/login");
+
+			// Espera un momento para que se carguen los resultados
+			System.Threading.Thread.Sleep(1000);
+
+			// Encuentra el cuadro de búsqueda
+			var btn = driver.FindElement(By.ClassName("btn btn-primary"));
+
+			btn.Click();
+			// Espera un momento para que se carguen los resultados
+			System.Threading.Thread.Sleep(1000);
+
+		}
+
+		public void Dispose()
+		{
+			// Cierra el navegador
+			driver.Quit();
+		}
+	}
+}
